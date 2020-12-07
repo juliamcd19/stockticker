4ae0bf4687a25e5d214ae35cd027ad040be77ef8
Julia McDowell <55772403+juliamcd19@users.noreply.github.com>
2020-12-07 13:51:28 -0500

    Add files via upload

--- read.js
+++ read.js
@@ -1,35 +1,35 @@
 console.log("Hello");
-const MongoClient = require('mongodb').MongoClient;
-var readline = require('readline');
-var fs = require('fs');
-var csvtojson = require("csvtojson");
+// const MongoClient = require('mongodb').MongoClient;
+// var readline = require('readline');
+// var fs = require('fs');
+// var csvtojson = require("csvtojson");
 //CHANGE URL
-const url = "mongodb+srv://juliamcd19:ballup360@cluster0.tn5ll.mongodb.net/stma?retryWrites=true&w=majority";
+// const url = "mongodb+srv://juliamcd19:ballup360@cluster0.tn5ll.mongodb.net/stma?retryWrites=true&w=majority";
 
-function main() 
-{
-    csvtojson()
-        .fromFile("companies-1.csv")
-        .then(csvData => {
-        console.log(csvData);
-    MongoClient.connect(url, function(err, db) {
-    if(err) { return console.log(err); }
-    console.log("into mongo");
+// function main() 
+// {
+//     csvtojson()
+//         .fromFile("companies-1.csv")
+//         .then(csvData => {
+//         console.log(csvData);
+//     MongoClient.connect(url, function(err, db) {
+//     if(err) { return console.log(err); }
+//     console.log("into mongo");
   
-        var dbo = db.db("stma");
-        var collection = dbo.collection('companies');
-        collection.remove({});
-        collection.insertMany(csvData, (err, res) => {
-            if (err) throw err;
+//         var dbo = db.db("stma");
+//         var collection = dbo.collection('companies');
+//         collection.remove({});
+//         collection.insertMany(csvData, (err, res) => {
+//             if (err) throw err;
 
-        console.log(`Inserted: ${res.insertedCount} rows`);
-        db.close();
-        });
-      }
-    );
-  });   
+//         console.log(`Inserted: ${res.insertedCount} rows`);
+//         db.close();
+//         });
+//       }
+//     );
+//   });   
  
-};
+// };
 
 var http = require('http');
 var port = process.env.PORT || 3000;
